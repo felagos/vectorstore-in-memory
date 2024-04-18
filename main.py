e1d152b172b3d538a70a60ba8f2a97dd153fd133
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-04-18 16:18:26 -0400

    Add langchain-community dep

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PythonLoader
+
+pdf = os.path.abspath("./file.pdf")
 
 if __name__ == '__main__':
     load_dotenv()
+
+    loader = PythonLoader(pdf)
