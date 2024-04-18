24635fb1b33b8e57a866aadc0ad23855e3baaaab
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-04-18 18:11:31 -0400

    Add langchain-openai dep and update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PythonLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 pdf = os.path.abspath("./file.pdf")
 
@@ -9,4 +11,12 @@ if __name__ == '__main__':
     load_dotenv()
 
     loader = PythonLoader(pdf)
+    loader.encoding = "latin-1"
     document = loader.load()
+
+    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=300, separator="\n")
+    docs = splitter.split_documents(document)
+
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(docs, embeddings)
+
