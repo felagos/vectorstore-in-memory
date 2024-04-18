faec31b4a3580648022f35c0e0231fe908b12f2d
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-04-18 16:22:03 -0400

    Add langchain dep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PythonLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 pdf = os.path.abspath("./file.pdf")
 
@@ -8,3 +9,4 @@ if __name__ == '__main__':
     load_dotenv()
 
     loader = PythonLoader(pdf)
+    document = loader.load()
